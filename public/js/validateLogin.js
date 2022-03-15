dee4605a47e323e34d9c755662e01e55339b2439
agustin.xo <aguz.kerplunk@gmail.com>
2022-03-15 09:57:13 -0300

    validation front en login / falta contraseña

---
+++ public/js/validateLogin.js
@@ -0,0 +1,46 @@
+window.addEventListener('load', function(){
+
+    let correo = document.querySelector('#correo')
+    let errorCorreo = document.querySelector('#error-correo')
+
+    let contraseña = document.querySelector('input#contra')
+    let errorContraseña = document.querySelector('div#error-contraseña')
+
+    let form = document.querySelector('#form')
+
+    let button = document.querySelector('#submit')
+
+
+
+    button.addEventListener('click', function(e){
+
+        e.preventDefault();
+
+        let errores = {}
+
+        if(correo.value === ''){
+            errores.correo = 'Este campo debe estar completo'
+        } else if (correo.value.includes('@') && correo.value.includes('.com') ){
+            return correo.value
+        } else {
+            errores.correo = 'Debes ingresar un email válido'
+        }
+        
+
+
+        if (contraseña.value === ''){
+            errores.contraseña = 'Debes ingresar una contraseña'
+        }
+
+
+        if (Object.keys(errores).length >= 1){
+            errorCorreo.innerHTML = errores.correo;
+            errorCorreo.innerHTML = errores.contraseña;
+        } else {
+            form.submit()
+        }
+
+
+    })
+
+})
