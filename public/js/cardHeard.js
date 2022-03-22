397a99260d995db688752f4114a0d506517c537a
Adela015 <ade.cervantes01@gmail.com>
2022-03-22 02:48:32 -0300

    heart en cards y modo oscuro a medias

---
+++ public/js/cardHeard.js
@@ -0,0 +1,10 @@
+let cambiarCorazon = document.getElementById('botonCorazon');
+
+function Toggle(){
+        if (cambiarCorazon.style.color =="red") {
+            cambiarCorazon.style.color = "pink"
+        }
+        else{
+            cambiarCorazon.style.color = "red"
+        }
+}
