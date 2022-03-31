588ee97c8bb6ef2cab5aab86f4df8df80ececc2d
agustin.xo <aguz.kerplunk@gmail.com>
2022-03-31 14:31:43 -0300

    eventos focus y blur en login

--- public/js/validateLogin.js
+++ public/js/validateLogin.js
@@ -2,10 +2,40 @@ window.addEventListener('load', function(){
 
 
     let form = document.querySelector('form')
+
     let correo = document.querySelector('#correo')
+    let errorCorreo = document.querySelector('#error-correo')
+
     let contraseña = document.querySelector('#contra')
+    let errorContraseña = document.querySelector('#error-contraseña')
+
     let erroresFront = document.querySelector('div.errores-front ul')
     let button = document.querySelector('#submit')
+
+
+
+    correo.addEventListener('focus', function() {
+        errorCorreo.innerHTML = ''
+    })
+
+    correo.addEventListener('blur', function() {
+        if (correo.value === ''){
+            errorCorreo.innerHTML += '<li> Este campo no puede estar vacío </li>'
+        }
+    })
+
+    contraseña.addEventListener('focus', function() {
+        errorContraseña.innerHTML = ''
+    })
+
+    contraseña.addEventListener('blur', function() {
+        if (contraseña.value === ''){
+            errorContraseña.innerHTML += '<li> Este campo no puede estar vacío </li>'
+        }
+    })
+
+
+
 
     button.addEventListener('click', function(e){
 
