f58b075c789cf1aed7af2e127bc8cf8cce160715
agustin.xo <aguz.kerplunk@gmail.com>
2022-03-31 14:59:53 -0300

    eventos focus y blur agregados en addproduct y editproduct

--- public/js/validateProduct.js
+++ public/js/validateProduct.js
@@ -1,8 +1,18 @@
 let form = document.querySelector('form');
+
 let nombre = document.querySelector('#name');
+let errorNombre = document.querySelector('#error-nombre')
+
 let artista = document.querySelector('#artist');
+let errorArtista = document.querySelector('#error-artista')
+
 let descripcion = document.querySelector('#description');
+let errorDescripcion = document.querySelector('#error-descripcion')
+
 let precio = document.querySelector('#price');
+let errorPrecio = document.querySelector('#error-precio')
+
+
 let imagen = document.querySelector('#image');
 let button = document.querySelector('#buttonSubmit');
 let ulErrores = document.querySelector('div.errores-front ul')
@@ -10,6 +20,50 @@ let ulErrores = document.querySelector('div.errores-front ul')
 window.addEventListener('load', function(){
 
     console.log('script funcionando')
+
+
+    nombre.addEventListener('focus', function() {
+        errorNombre.innerHTML = ''
+    })
+
+    nombre.addEventListener('blur', function() {
+        if (nombre.value === ''){
+            errorNombre.innerHTML += '<li> Este campo no puede estar vacío </li>'
+        }
+    })
+
+
+    artista.addEventListener('focus', function() {
+        errorArtista.innerHTML = ''
+    })
+
+    artista.addEventListener('blur', function() {
+        if (artista.value === ''){
+            errorArtista.innerHTML += '<li> Este campo no puede estar vacío </li>'
+        }
+    })
+
+
+    descripcion.addEventListener('focus', function() {
+        errorDescripcion.innerHTML = ''
+    })
+
+    descripcion.addEventListener('blur', function() {
+        if (descripcion.value === ''){
+            errorDescripcion.innerHTML += '<li> Este campo no puede estar vacío </li>'
+        }
+    })
+
+
+    precio.addEventListener('focus', function() {
+        errorPrecio.innerHTML = ''
+    })
+
+    precio.addEventListener('blur', function() {
+        if (precio.value === ''){
+            errorPrecio.innerHTML += '<li> Este campo no puede estar vacío </li>'
+        }
+    })
 
 
     
